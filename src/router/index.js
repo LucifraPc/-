2751d6d3e08deb34adaad6820b88d515c7c86197
ppc <978648119@qq.com>
2018-04-02 13:04:16 +0800

    电销系统

---
+++ src/router/index.js
@@ -0,0 +1,131 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+// in development-env not use lazy-loading, because lazy-loading too many pages will cause webpack hot update too slow. so only in production use lazy-loading;
+// detail: https://panjiachen.github.io/vue-element-admin-site/#/lazy-loading
+
+Vue.use(Router)
+
+/* Layout */
+import Layout from '../views/layout/Layout'
+
+/**
+* hidden: true                   if `hidden:true` will not show in the sidebar(default is false)
+* alwaysShow: true               if set true, will always show the root menu, whatever its child routes length
+*                                if not set alwaysShow, only more than one route under the children
+*                                it will becomes nested mode, otherwise not show the root menu
+* redirect: noredirect           if `redirect:noredirect` will no redirct in the breadcrumb
+* name:'router-name'             the name is used by <keep-alive> (must set!!!)
+* meta : {
+    title: 'title'               the name show in submenu and breadcrumb (recommend set)
+    icon: 'svg-name'             the icon show in the sidebar,
+  }
+**/
+export const constantRouterMap = [
+  { path: '/login', component: () => import('@/views/login/index'), hidden: true },
+  { path: '/404', component: () => import('@/views/404'), hidden: true },
+
+  {
+    path: '/',
+    component: Layout,
+    redirect: '/customer-management'
+    // name: 'Dashboard',
+    // hidden: true,
+    // children: [{
+    //   path: 'dashboard',
+    //   component: () => import('@/views/dashboard/index')
+    // }]
+  },
+
+  {
+    path: '/customer-management',
+    component: Layout,
+    redirect: '/customer-management/table',
+    name: '客户管理',
+    meta: { title: '客户管理', icon: 'example' },
+    children: [
+      {
+        path: 'table',
+        name: 'Table',
+        component: () => import('@/views/CustomerManagement/table'),
+        meta: { title: 'Table', icon: 'table' }
+      },
+      {
+        path: 'tree',
+        name: 'Tree',
+        component: () => import('@/views/CustomerManagement/tree'),
+        meta: { title: 'Tree', icon: 'tree' }
+      }
+    ]
+  },
+
+  {
+    path: '/resource-allocation',
+    component: Layout,
+    redirect: '/resource-allocation/table',
+    name: '资源分配',
+    meta: { title: '资源分配', icon: 'example' },
+    children: [
+      {
+        path: 'table',
+        name: 'Table',
+        component: () => import('@/views/ResourceAllocation/table'),
+        meta: { title: 'Table', icon: 'table' }
+      },
+      {
+        path: 'tree',
+        name: 'Tree',
+        component: () => import('@/views/ResourceAllocation/tree'),
+        meta: { title: 'Tree', icon: 'tree' }
+      }
+    ]
+  },
+  
+
+  {
+    path: '/customer-relationship',
+    component: Layout,
+    children: [
+      {
+        path: 'index',
+        name: '客户公海',
+        component: () => import('@/views/CustomerRelationship/table'),
+        meta: { title: '客户公海', icon: 'form' }
+      }
+    ]
+  },
+
+  {
+    path: '/system-settings',
+    component: Layout,
+    children: [
+      {
+        path: 'index',
+        name: '系统设置',
+        component: () => import('@/views/SystemSettings/404'),
+        meta: { title: '系统设置', icon: 'form' }
+      }
+    ]
+  },
+  {
+    path: '/data-report',
+    component: Layout,
+    children: [
+      {
+        path: 'index',
+        name: '数据报表',
+        component: () => import('@/views/DataReport/index'),
+        meta: { title: '数据报表', icon: 'form' }
+      }
+    ]
+  },
+
+  { path: '*', redirect: '/404', hidden: true }
+]
+
+export default new Router({
+  // mode: 'history', //后端支持可开
+  scrollBehavior: () => ({ y: 0 }),
+  routes: constantRouterMap
+})
+
