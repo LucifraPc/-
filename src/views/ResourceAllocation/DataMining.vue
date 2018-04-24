516aecec8f8661315c20703e42dcd68fb9f3a6d0
ppc <978648119@qq.com>
2018-04-24 17:22:05 +0800

    脏数据处理

--- src/views/ResourceAllocation/DataMining.vue
+++ src/views/ResourceAllocation/DataMining.vue
@@ -112,11 +112,7 @@
           }
         ],
         value: "",
-        tableData: [{
-          date: "2016-05-02",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1518 弄"
-        }]
+        tableData: []
       };
     },
     watch: {
