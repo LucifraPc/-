15c3aab86fa58ffa15b2c7db35a4a1191359d09e
zhl <zhl5632@163.com>
2018-04-25 15:17:47 +0800

    修改菜单显示

--- src/views/layout/components/Sidebar/SidebarItem.vue
+++ src/views/layout/components/Sidebar/SidebarItem.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="menu-wrapper">
     <template v-for="item in routes" v-if="!item.hidden&&item.children">
-      <!-- <span>{{item}}</span> -->
-      <router-link class="1111" v-if="item.children.length===1 && !item.children[0].children && !item.alwaysShow" :to="item.path+'/'+item.children[0].path"
+
+      <router-link  v-show="!(item.children[0].name!='客户公海' && roleCrm=='MEMBER')" v-if="item.children.length===1 && !item.children[0].children && !item.alwaysShow" :to="item.path+'/'+item.children[0].path"
         :key="item.children[0].name">
         <el-menu-item :index="item.path+'/'+item.children[0].path" :class="{'submenu-title-noDropdown':!isNest}">
           <svg-icon v-if="item.children[0].meta&&item.children[0].meta.icon" :icon-class="item.children[0].meta.icon"></svg-icon>
@@ -10,7 +10,7 @@
         </el-menu-item>
       </router-link>
 
-      <el-submenu  v-else :index="item.name||item.path" :key="item.name" class="1111">
+      <el-submenu  v-show="!(item.name!='客户管理' && roleCrm=='MEMBER')"  v-else :index="item.name||item.path" :key="item.name" >
         <template slot="title">
           <svg-icon v-if="item.meta&&item.meta.icon" :icon-class="item.meta.icon"></svg-icon>
           <span v-if="item.meta&&item.meta.title">{{item.meta.title}}</span>
@@ -32,7 +32,6 @@
     </template>
   </div>
 </template>
-<!-- v-show="(roleCrm!='MEMBER' && item.name!='系统设置') || (roleCrm!='MEMBER' && item.name!='数据报表')" -->
 <script>
   export default {
     name: 'SidebarItem',
